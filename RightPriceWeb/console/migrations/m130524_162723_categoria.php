d2727f0bff9d28db499a25fb30084272cace1018
Marcos Ferreira <whiteknight96@hotmail.com>
2020-11-03 19:36:34 +0100

    users

---
+++ RightPriceWeb/console/migrations/m130524_162723_categoria.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m130524_162723_categoria
+ */
+class m130524_162723_categoria extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('post', [
+            'id' => $this->primaryKey(),
+            'Nome_Categoria' => $this->string()->notNull(),
+        ]);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m130524_162723_categoria cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m130524_162723_categoria cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
